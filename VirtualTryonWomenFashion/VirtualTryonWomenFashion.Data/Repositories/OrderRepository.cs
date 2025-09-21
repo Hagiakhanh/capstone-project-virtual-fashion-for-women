f243748c6a4c038c78d7472f55175741d997de7c
NguyenDHung <hungndse171325@fpt.edu.vn>
2025-09-21 23:32:49 +0700

    API manage order for staff

--- VirtualTryonWomenFashion/VirtualTryonWomenFashion.Data/Repositories/OrderRepository.cs
+++ VirtualTryonWomenFashion/VirtualTryonWomenFashion.Data/Repositories/OrderRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,6 +15,13 @@ namespace VirtualTryonWomenFashion.Data.Repositories
     {
         public OrderRepository(VirtualTryonWomenFashionContext context) : base(context)
         {
+        }
+
+        public async Task<Order?> GetOrderByOrderID(int orderID)
+        {
+            return await _context.Orders.Include(x => x.OrderDetails)
+                .ThenInclude(x => x.ProductVariant)
+                .SingleOrDefaultAsync(x => x.OrderId == orderID);
         }
     }
 }
