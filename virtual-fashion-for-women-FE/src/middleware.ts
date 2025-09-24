fe5fff282cb5aa19f3fccb70fc69c2bd624d5fa9
NguyenDHung <hungndse171325@fpt.edu.vn>
2025-09-24 17:06:08 +0700

    Config middleware and call api login

--- virtual-fashion-for-women-FE/src/middleware.ts
+++ virtual-fashion-for-women-FE/src/middleware.ts
@@ -1,30 +1,62 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { jwtVerify } from "jose";
 
-export function middleware(req: NextRequest) {
+export async function middleware(req: NextRequest) {
   const token = req.cookies.get("token")?.value;
-  const role = req.cookies.get("role")?.value;
+  //const role = req.cookies.get("role")?.value;
   const url = req.nextUrl.clone();
-  // if (!token || role == null) {
-  //   if (url.pathname.startsWith("/admin")) {
-  //     url.pathname = "/admin/login";
-  //   } else if (url.pathname.startsWith("/staff")) {
-  //     url.pathname = "/staff/login";
-  //   }
-  //   return NextResponse.redirect(url);
-  // }
-  // if (url.pathname.startsWith("/admin") && role !== null && role !== "admin") {
-  //   url.pathname = "/unauthorized";
-  //   return NextResponse.redirect(url);
-  // }
+  console.log('----------Check middleware runing----------');
+  console.log('Token:', token);
 
-  // if (url.pathname.startsWith("/staff") && role !== null && role !== "staff") {
-  //   url.pathname = "/unauthorized";
-  //   return NextResponse.redirect(url);
-  // }
+  // Kiểm tra xem người dùng đã đăng nhập hay chưa
+  if (token && req.nextUrl.pathname === '/login') {
+    return NextResponse.redirect(new URL('/', req.url));
+  }
+  // Các route cho customer
+  const customerRoutes = [
+    "/cart"
+  ];
+  const isProtectedRoute = url.pathname.startsWith("/admin") || url.pathname.startsWith("/staff") || customerRoutes.some((route) => url.pathname.startsWith(route));
+  if (isProtectedRoute && !token) {
+    return NextResponse.redirect(new URL('/login', req.url));
+  }
+
+  if (token) {
+    try {
+      const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+      const decoded = await jwtVerify(token, secret);
+      const role = decoded.payload.role;
+
+      // Rule cho admin
+      if (url.pathname.startsWith("/admin") && role !== "Admin") {
+        url.pathname = "/unauthorized";
+        return NextResponse.redirect(url);
+      }
+
+      // Rule cho staff
+      if (url.pathname.startsWith("/staff") && role !== "Staff") {
+        url.pathname = "/unauthorized";
+        return NextResponse.redirect(url);
+      }
+
+      // Rule cho customer
+      if (customerRoutes.some((route) => url.pathname.startsWith(route)) && role !== "Customer") {
+        url.pathname = "/unauthorized";
+        return NextResponse.redirect(url);
+      }
+
+    } catch (error) {
+      console.log('Invalid token or no token present');
+      const response = NextResponse.redirect(new URL('/login', req.url));
+      response.cookies.delete("token");
+      return response;
+    }
+  }
+
   return NextResponse.next();
 }
 
-// export const config = {
-//   matcher: ["/admin/:path*", "/staff/:path*"], // áp dụng cho route nào
-// };
+export const config = {
+  matcher: ["/admin/:path*", "/staff/:path*", "/login"], // áp dụng cho route nào
+};
