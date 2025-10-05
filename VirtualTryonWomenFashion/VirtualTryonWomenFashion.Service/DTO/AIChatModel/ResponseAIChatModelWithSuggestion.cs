0607f75f8ccbd71fd4a7002c970043009eadddde
PhucLocNguyen <loc19992003bca@gmail.com>
2025-10-05 23:58:18 +0700

    Update message and AI Conversation

---
+++ VirtualTryonWomenFashion/VirtualTryonWomenFashion.Service/DTO/AIChatModel/ResponseAIChatModelWithSuggestion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualTryonWomenFashion.Service.DTO.AIChatModel
+{
+    public class ResponseAIChatModelWithSuggestion
+    {
+        public bool IsAiresponse { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public List<ProductSuggestionDTO>? Components { get; set; } = new List<ProductSuggestionDTO>();
+    }
+}
