c1022dbec4b27fe61c5bad5c38362059d7fb455a
PhucLocNguyen <loc19992003bca@gmail.com>
2025-10-13 17:30:09 +0700

    Add new column for size table

---
+++ VirtualTryonWomenFashion/VirtualTryonWomenFashion.Service/DTO/Size/RequestCreateSizeModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualTryonWomenFashion.Service.DTO.Size
+{
+    public class RequestCreateSizeModel
+    {
+        public string SizeCode { get; set; }
+
+        public double? MinHeight { get; set; }
+
+        public double? MaxHeight { get; set; }
+
+        public double? MinBust { get; set; }
+
+        public double? MaxBust { get; set; }
+
+        public double? MinWaist { get; set; }
+
+        public double? MaxWaist { get; set; }
+
+        public double? MinHips { get; set; }
+
+        public double? MaxHips { get; set; }
+    }
+}
