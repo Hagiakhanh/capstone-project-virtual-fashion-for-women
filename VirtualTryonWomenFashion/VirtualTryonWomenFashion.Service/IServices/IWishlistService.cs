b7ec6e023cfbbecb8a0854b565a2c283fab6af59
NguyenDHung <hungndse171325@fpt.edu.vn>
2025-10-15 21:04:40 +0700

    Api for wishlist function

--- VirtualTryonWomenFashion/VirtualTryonWomenFashion.Service/IServices/IWishlistService.cs
+++ VirtualTryonWomenFashion/VirtualTryonWomenFashion.Service/IServices/IWishlistService.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VirtualTryonWomenFashion.Data.Commons;
+using VirtualTryonWomenFashion.Data.Models;
+using VirtualTryonWomenFashion.Service.DTO.Wishlist;
+using VirtualTryonWomenFashion.Service.Helpers;
 
 namespace VirtualTryonWomenFashion.Service.IServices
 {
     public interface IWishlistService
     {
+        public Task<MessageModelWithData<List<Wishlist>>> GetAllWishList(PaginationParameter paginationParameter);
+        public Task<MessageModel> AddProductToWishlist(RequestAddWishlist requestAddWishlist);
+        public Task<MessageModel> RemoveProductFromWishlist(int wishlistId);
     }
 }
