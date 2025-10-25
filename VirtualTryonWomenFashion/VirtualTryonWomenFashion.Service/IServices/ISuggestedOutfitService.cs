798167d618edafe28e50170e3772b810f207a9e1
PhucLocNguyen <loc19992003bca@gmail.com>
2025-10-26 01:03:59 +0700

    Upload suggestion outfit get by conversation id

--- VirtualTryonWomenFashion/VirtualTryonWomenFashion.Service/IServices/ISuggestedOutfitService.cs
+++ VirtualTryonWomenFashion/VirtualTryonWomenFashion.Service/IServices/ISuggestedOutfitService.cs
@@ -5,12 +5,13 @@ using System.Text;
 using System.Threading.Tasks;
 using VirtualTryonWomenFashion.Data.Commons;
 using VirtualTryonWomenFashion.Data.Models;
+using VirtualTryonWomenFashion.Service.DTO.SuggestedOutfit;
 
 namespace VirtualTryonWomenFashion.Service.IServices
 {
     public interface ISuggestedOutfitService
     {
-        public Task<Pagination<SuggestedOutfit>> GetSuggestedOutfitsAsync(PaginationParameter paginationParameter, int? AIConversationID);
+        public Task<Pagination<ResponseSuggestedOutfitModel>> GetSuggestedOutfitsAsync(PaginationParameter paginationParameter, int? AIConversationID);
         public Task<SuggestedOutfit> CreateSuggestedOutfit(SuggestedOutfit suggestedOutfit);
     }
 }
---
+++ VirtualTryonWomenFashion/VirtualTryonWomenFashion.Service/Mappers/SuggestedOutfitProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualTryonWomenFashion.Data.Models;
+using VirtualTryonWomenFashion.Service.DTO.SuggestedOutfit;
+
+namespace VirtualTryonWomenFashion.Service.Mappers
+{
+    public class SuggestedOutfitProfile : Profile
+    {
+        public SuggestedOutfitProfile()
+        {
+            CreateMap<SuggestedOutfit, ResponseSuggestedOutfitModel>();
+        }
+    }
+}
