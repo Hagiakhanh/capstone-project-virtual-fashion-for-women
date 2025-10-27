1890e1ebf2c5bb4152370b0be2dae002a98c00ab
NguyenDHung <hungndse171325@fpt.edu.vn>
2025-10-27 22:00:03 +0700

    Add redis service for catching and api logout

---
+++ VirtualTryonWomenFashion/VirtualTryonWomenFashion.Service/Services/RedisCacheService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using VirtualTryonWomenFashion.Service.IServices;
+
+namespace VirtualTryonWomenFashion.Service.Services
+{
+    public class RedisCacheService : IRedisCacheService
+    {
+        private readonly IDistributedCache _cache;
+
+        public RedisCacheService(IDistributedCache cache)
+        {
+            _cache = cache;
+        }
+
+        public async Task<T?> GetData<T>(string key)
+        {
+            var data = await _cache.GetStringAsync(key);
+            if (data == null)
+            {
+                return default(T?);
+            }
+            return JsonSerializer.Deserialize<T>(data);
+        }
+
+        public async Task SetData<T>(string key, T value, TimeSpan? expiryTime)
+        {
+            var option = new DistributedCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = expiryTime
+            };
+            await _cache?.SetStringAsync(key, JsonSerializer.Serialize(value), option);
+        }
+    }
+}
