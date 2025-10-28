66f55626caab8981d86b26ca8fa8c3c7c6930b5a
NguyenBaDat <bmbadat2234@gmail.com>
2025-10-28 23:41:05 +0700

    Create StatusLog

---
+++ VirtualTryonWomenFashion/VirtualTryonWomenFashion.Service/Mappers/StatusLogMapper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualTryonWomenFashion.Data.Models;
+using VirtualTryonWomenFashion.Service.DTO.StatusLog;
+
+namespace VirtualTryonWomenFashion.Service.Mappers
+{
+    public static class StatusLogMapper
+    {
+        public static StatusLog ToStatusLogFromRequest(this RequestCreateStatusLog requestCreateStatusLog)
+        {
+            return new StatusLog()
+            {
+                OrderId = requestCreateStatusLog.OrderId,
+                Status = requestCreateStatusLog.Status,
+                UpdateDate = requestCreateStatusLog.UpdateAt,
+            };
+        }
+    }
+}
