120657da2ad8a037d94cd003c06750024247bc30
Ha Gia Khanh <gkhanh903@gmail.com>
2025-10-30 19:42:25 +0700

    package lens

--- VirtualTryonWomenFashion/VirtualTryonWomenFashion.Service/IServices/INotificationService.cs
+++ VirtualTryonWomenFashion/VirtualTryonWomenFashion.Service/IServices/INotificationService.cs
@@ -4,19 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using VirtualTryonWomenFashion.Data.Commons;
-using VirtualTryonWomenFashion.Service.DTO.Notification;
 using VirtualTryonWomenFashion.Service.Helpers;
 
 namespace VirtualTryonWomenFashion.Service.IServices
 {
     public interface INotificationService
     {
-        Task<int> CountNotificationIsNotRead(int userId);
-        Task<MessageModel> CreateNotification(List<CreateNotificationRequest> requestCreateNotifications);
-        Task<Pagination<ResponseNotification>> GetAllNotificationByUserId(int userId,
-            PaginationParameter paginationParameter);
-        Task<ResponseNotification> GetNotificationById(int notificationId);
-        Task<bool> MarkAllNotificationsAsRead();
-        Task<ResponseNotification?> MarkNotificationAsRead(int notificationId);
     }
 }
