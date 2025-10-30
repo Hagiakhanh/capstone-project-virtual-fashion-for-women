730eeb62a33128dce2e0a493ad17b1435452c87a
Ha Gia Khanh <gkhanh903@gmail.com>
2025-10-30 21:28:47 +0700

    add filter and sort admin product page

--- virtual-fashion-for-women-FE/src/components/Category/CategoryCard.tsx
+++ virtual-fashion-for-women-FE/src/components/Category/CategoryCard.tsx
@@ -32,13 +32,13 @@ export default function CategoryCard({
                         onClick={onDelete}
                         className="px-4 py-2 text-sm font-medium text-red-700 bg-red-100 rounded-lg hover:bg-red-200 transition-colors"
                     >
-                        Delete
+                        Xóa
                     </button>
                     <button
                         onClick={onEdit}
                         className="px-4 py-2 text-sm font-medium text-blue-700 bg-blue-100 rounded-lg hover:bg-blue-200 transition-colors"
                     >
-                        Edit
+                        Sửa
                     </button>
                 </div>
             </div>
