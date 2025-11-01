91edbd2f2055c512482d3513eda60814710f7e1e
NguyenDHung <hungndse171325@fpt.edu.vn>
2025-11-01 17:08:56 +0700

    List order refund customer

--- virtual-fashion-for-women-FE/src/components/Customer/CustomerSidebar.tsx
+++ virtual-fashion-for-women-FE/src/components/Customer/CustomerSidebar.tsx
@@ -2,13 +2,14 @@
 
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { User, Package, MessageCircle, CreditCard, Shirt, History, BotMessageSquare } from 'lucide-react';
+import { User, Package, MessageCircle, CreditCard, Shirt, History, BotMessageSquare, CalendarSync } from 'lucide-react';
 
 const navItems = [
     { name: 'Thông tin tài khoản', href: '/account', icon: User },
     { name: 'Đơn hàng của bạn', href: '/account/orders', icon: Package },
     { name: 'Trò chuyện với AI', href: '/account/aiConversations', icon: BotMessageSquare },
     { name: 'Lịch sử giao dịch', href: '/account/transactions', icon: CreditCard },
+    { name: 'Yêu cầu hoàn hàng', href: '/account/order-refund', icon: CalendarSync },
     { name: 'Yêu cầu hỗ trợ', href: '/account/chats', icon: MessageCircle },
     { name: 'Lịch sử hỗ trợ', href: '/account/history-chats', icon: History },
     { name: 'Lịch sử thử đồ', href: '/account/try-on-history', icon: Shirt },
