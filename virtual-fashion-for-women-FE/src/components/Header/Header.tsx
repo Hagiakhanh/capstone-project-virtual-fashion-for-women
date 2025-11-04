c7c1d562df863c9f17e0794df9fa5f3fcb0a64ad
PhucLocNguyen <loc19992003bca@gmail.com>
2025-11-04 22:50:24 +0700

    test

--- virtual-fashion-for-women-FE/src/components/Header/Header.tsx
+++ virtual-fashion-for-women-FE/src/components/Header/Header.tsx
@@ -311,6 +311,17 @@ function HeaderComponent() {
                         className="relative cursor-pointer"
                         onClick={() => router.push('/cart')}
                      >
+                        <ShoppingCartOutlined className="text-3xl" />
+                        {cartCount > 0 && (
+                           <span className="absolute -top-2 -right-2 bg-red-600 text-white text-xs font-bold rounded-full min-w-[18px] h-[18px] flex items-center justify-center border-2 border-white">
+                              {cartCount > 99 ? '99+' : cartCount}
+                           </span>
+                        )}
+                     </div>
+                     <div
+                        className="relative cursor-pointer"
+                        onClick={() => router.push('/cart')}
+                     >
                         <WalletOutlined className="text-2xl" />
                      </div>
                      <div
