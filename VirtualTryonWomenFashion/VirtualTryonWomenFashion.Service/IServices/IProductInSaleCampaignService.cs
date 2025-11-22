fcd28bfec142540568e74904886d0473a994b7f4
PhucLocNguyen <loc19992003bca@gmail.com>
2025-11-22 12:23:48 +0700

    add product in sale campaign endpoint

--- VirtualTryonWomenFashion/VirtualTryonWomenFashion.Service/IServices/IProductInSaleCampaignService.cs
+++ VirtualTryonWomenFashion/VirtualTryonWomenFashion.Service/IServices/IProductInSaleCampaignService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VirtualTryonWomenFashion.Data.Commons;
 using VirtualTryonWomenFashion.Data.Models;
 using VirtualTryonWomenFashion.Service.DTO.Product;
 using VirtualTryonWomenFashion.Service.DTO.ProductInSaleCampaign;
@@ -19,6 +20,7 @@ namespace VirtualTryonWomenFashion.Service.IServices
         public Task<bool> InsertListProductInSaleCampaign(List<ProductInSaleCampaign> listProductInSaleCampaign);
         public Task<List<ResponseGetProductInSaleCampaign>> GetListProductBasedCampaignID(int campaignID);
         public Task<List<ResponseGetProductInSaleCampaign>> GetProductInSaleCampaign(string productId);
+        public Task<ResponsePaginationModel<List<ResponseGetProductInSaleCampaign>>> GetListProductBasedCampaignIDPagination(int campaignId, PaginationParameter paginationParameter);
         Task<Dictionary<string, decimal>> GetPricesOfProductsInActiveCampaignAsync(List<string> productIds);
         public Task<ResponseGetProductInSaleCampaign> GetPriceOfProductInActiveCampaign(string productId);
         public Task<bool> BulkDeleteProductInCampaign(int campaignID, List<string> listProductID);
