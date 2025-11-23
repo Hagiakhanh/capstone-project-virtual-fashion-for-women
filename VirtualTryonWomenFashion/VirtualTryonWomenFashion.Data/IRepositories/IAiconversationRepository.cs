e01183b60db577ead49c179446419b387a9cc380
Ha Gia Khanh <gkhanh903@gmail.com>
2025-11-23 18:14:11 +0700

    feat: total count ai conversation, top total suggested product

--- VirtualTryonWomenFashion/VirtualTryonWomenFashion.Data/IRepositories/IAiconversationRepository.cs
+++ VirtualTryonWomenFashion/VirtualTryonWomenFashion.Data/IRepositories/IAiconversationRepository.cs
@@ -5,10 +5,14 @@ using System.Text;
 using System.Threading.Tasks;
 using VirtualTryonWomenFashion.Data.GenericRepository;
 using VirtualTryonWomenFashion.Data.Models;
+using VirtualTryonWomenFashion.Data.Repositories;
 
 namespace VirtualTryonWomenFashion.Data.IRepositories
 {
     public interface IAiconversationRepository : IGenericRepository<Aiconversation>
     {
+        Task<int> GetTotalConversationsAsync();
+        Task<int> GetTotalUsersUsedAIAsync();
+        Task<List<AiConversationChartDto>> GetConversationChartAsync(DateTime startDate, DateTime endDate);
     }
 }
--- VirtualTryonWomenFashion/VirtualTryonWomenFashion.Data/IRepositories/ISuggestedOutfitRepository.cs
+++ VirtualTryonWomenFashion/VirtualTryonWomenFashion.Data/IRepositories/ISuggestedOutfitRepository.cs
@@ -5,13 +5,18 @@ using System.Text;
 using System.Threading.Tasks;
 using VirtualTryonWomenFashion.Data.GenericRepository;
 using VirtualTryonWomenFashion.Data.Models;
+using VirtualTryonWomenFashion.Data.Repositories;
 
 namespace VirtualTryonWomenFashion.Data.IRepositories
 {
     public interface ISuggestedOutfitRepository : IGenericRepository<SuggestedOutfit>
     {
-
         public Task<bool> CreateSuggestedOutfit(SuggestedOutfit requestModel);
+        Task<int> GetTotalSuggestedItemsAsync();
+        Task<List<TopSuggestedProductDto>> GetTopSuggestedProductsAsync(
+            DateTime start,
+            DateTime end,
+            int top);
     }
 
 }
